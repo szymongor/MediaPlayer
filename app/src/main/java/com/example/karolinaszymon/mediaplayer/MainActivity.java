83936cc78bb9a148064b6bc4ff08f070fa9a1022
Karo <k.chrzaszcz12@gmail.com>
2016-11-29 13:59:31 +0100

    Change Button to ImageButton

--- app/src/main/java/com/example/karolinaszymon/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/karolinaszymon/mediaplayer/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.karolinaszymon.mediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton buttonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUIComponents();
     }
+
+    private void initUIComponents() {
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(clickPlay);
+    }
+
+    private View.OnClickListener clickPlay = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //do uzupełnienia
+        }
+    };
 }
